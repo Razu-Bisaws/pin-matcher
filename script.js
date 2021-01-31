3e4763252b36d342eaba637cc88e235d69e7895c
Razu-Biswas <sreekantaraju3421@gmail.com>
2021-01-31 18:48:30 +0600

    commit2 correction

---
+++ script.js
@@ -0,0 +1,53 @@
+function getPin() {
+    const random = Math.random() * 10000;
+    const pin = (random + '').split('.')[0];
+    if (pin.length === 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+
+// display generated pin
+function generatePin() {
+    const pinInput = document.getElementById('pin');
+    pinInput.value = getPin();
+}
+
+// handle calculator button event
+const buttonContainer = document.getElementById('digits-container');
+buttonContainer.addEventListener('click', function (event) {
+    const digit = event.target.innerText;
+    if (isNaN(digit)) {
+        // handle backspace
+        // handle cleare
+        if (digit === 'C') {
+            const typedInput = document.getElementById('typed-pin');
+            typedInput.value = '';
+        }
+    }
+    else {
+        const typedInput = document.getElementById('typed-pin');
+        typedInput.value = typedInput.value + digit;
+    }
+})
+
+// verifypin handler
+function verifyPin() {
+    const pin = document.getElementById('pin').value;
+    const typedPin = document.getElementById('typed-pin').value;
+    if (pin === typedPin) {
+        displayMatchResult('block', 'none');
+    }
+    else {
+        displayMatchResult('none', 'block');
+    }
+}
+
+function displayMatchResult(correctStatus, inCorrectStatus) {
+    const correct = document.getElementById('correct-pin');
+    correct.style.display = correctStatus;
+    const incorrect = document.getElementById('incorrect-pin');
+    incorrect.style.display = inCorrectStatus;
+}
